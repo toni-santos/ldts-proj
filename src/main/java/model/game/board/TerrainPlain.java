d058ac66b44350521426ec50c8d157769fa87595
toni-santos <72560332+toni-santos@users.noreply.github.com>
2022-01-03 01:56:51 +0000

    Revert "Terrain types"

--- src/main/java/model/game/board/TerrainPlain.java
+++
@@ -1,9 +0,0 @@
-package model.game.board;
-
-import model.Position;
-
-public class TerrainPlain extends Terrain{
-    TerrainPlain(Position pos) {
-        super(pos);
-    }
-}
--- src/main/java/model/game/board/TerrainWater.java
+++
@@ -1,9 +0,0 @@
-package model.game.board;
-
-import model.Position;
-
-public class TerrainWater extends Terrain {
-    public TerrainWater(Position pos) {
-        super(pos);
-    }
-}
