3b664b64cebbd7ad4e69ce51a9cde87169f479fd
pedrosilva17 <pedroafsuper@gmail.com>
2022-01-05 21:59:14 +0000

    Started working on gui (terminal) with lanterna

---
+++ src/main/java/gui/GUI.java
@@ -0,0 +1,26 @@
+package gui;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.Terminal;
+
+public class GUI {
+
+    public GUI(Screen screen) {
+
+    }
+
+    private Screen createScreen (Terminal terminal) {
+
+    }
+
+    public void clear() {
+
+    }
+
+    public void close() {
+
+    }
+
+    public void update() {
+
+    }
+}
