05f1339c670d5a84f102549f4a9ad4b28ffe4b8f
toni-santos <a.oliveirasantos122@gmail.com>
2022-01-06 01:42:27 +0000

    refactored ability to be properly implemented with an enum

--- src/main/java/model/game/entity/Ability.java
+++ src/main/java/model/game/entity/Ability.java
@@ -2,54 +2,32 @@ package model.game.entity;
 
 import org.javatuples.Pair;
 
-public enum Name {
-    PUNCH,
-    LONG_RANGE_MISSILE,
-    GRENADE,
-    ARMAGEDDON
-}
+public enum Ability {
 
-public class Ability {
-    String name;
-    Integer range;
-    String description;
-    Integer damage;
+    PUNCH("Punch", 1, "Punches with extreme force, sending enemies 2 squares back!", 1, new Pair<String, Integer>("back", 2)),
+    LONG_RANGE_MISSILE("Long Range Missile", 6, "Sends a deadly missile dealing more damage but not pushing them!", 1, null),
+    GRENADE("Grenade",3, "Lobs a grenade at a given position, pushing everyone around it!", 1, null),
+    ARMAGEDDON("Armageddon", 0, "Ends the level...", 21, null);
+
+    private final String name;
+    private final int range;
+    private final String description;
+    private final int damage;
     /**
      * @brief This pair is created using the library javatuples and is used to store a String of the directions in which the attack pushes enemies and how far it does
      */
-    Pair<String, Integer> push;
+    private final Pair<String, Integer> push;
 
-    public Ability(Name name) {
-        switch (name) {
-            case PUNCH:
-                this.name = "Punch";
-                this.range = 1;
-                this.description = "Punches with extreme force, sending enemies 2 squares back!";
-                this.damage = 1;
-                this.push.setAt0("back").setAt1(2);
-                break;
-            case LONG_RANGE_MISSILE:
-                this.name = "Long Range Missile";
-                this.range = 10;
-                this.description = "Sends a deadly missile dealing more damage but not pushing them!";
-                this.damage = 2;
-                this.push = null;
-                break;
-            case GRENADE:
-                this.name = "Grenade";
-                this.range = 3;
-                this.description = "Lobs a grenade at a given position, pushing everyone around it!";
-                this.damage = 1;
-                this.push.setAt0("N-E-W-S").setAt1(1);
-                break;
-            case ARMAGEDDON:
-                this.name = "ARMAGEDDON";
-                this.range = 0;
-                this.description = "Ends the level...";
-                this.damage = -256;
-                this.push = null;
-                break;
-        }
+    private Ability(String name, int range, String description, int damage, Pair<String, Integer> push) {
+        this.name = name;
+        this.range = range;
+        this.description = description;
+        this.damage =  damage;
+        this.push = push;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public Integer getDamage() {
