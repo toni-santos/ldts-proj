ffdd9c84503ba8cee0e52c486b019c472aa91f2f
pedrosilva17 <pedroafsuper@gmail.com>
2022-01-08 18:21:22 +0000

    Documented methods and explained tests

--- src/main/java/gui/GUI.java
+++ src/main/java/gui/GUI.java
@@ -9,24 +9,49 @@ import model.Position;
 import java.io.IOException;
 
 public class GUI {
+    /**
+     * @note Changes made to the GUI through these methods are only visible after calling the refresh method.
+     */
+
     private Screen screen;
 
+    /**
+     * @brief Constructor of the GUI.
+     * @param screen the Lanterna screen that forms the GUI.
+     */
     public GUI(Screen screen) {
         this.screen = screen;
     }
 
+    /**
+     * @brief Clears the GUI.
+     */
     public void clear() {
         screen.clear();
     }
 
+    /**
+     * @brief Closes the GUI.
+     * @throws IOException
+     */
     public void close() throws IOException {
         screen.close();
     }
 
+    /**
+     * @brief updates the screen by taking the contents from the back buffer to the front buffer.
+     * @throws IOException
+     */
     public void refresh() throws IOException {
         screen.refresh();
     }
 
+    /**
+     * @brief Creates a new Screen object using the TerminalScreen class.
+     * @param terminal the terminal under the screen to be created.
+     * @return a Screen object.
+     * @throws IOException
+     */
     private Screen createScreen(Terminal terminal) throws IOException {
         Screen screen = new TerminalScreen(terminal);
         screen.startScreen();
@@ -35,23 +60,48 @@ public class GUI {
         return screen;
     }
 
+    /**
+     * @brief Draws a new entity to the back buffer of the GUI.
+     * @param x the x coordinate of the entity to be created.
+     * @param y the y coordinate of the entity to be created.
+     * @param c the character that will represent the entity.
+     */
+
     private void drawEntity(int x, int y, char c) {
         TextGraphics tg = screen.newTextGraphics();
         tg.putString(x, y, "" + c);
     }
 
+    /**
+     * @brief Draws a new alien using the drawEntity method.
+     * @param position the position (x and y coordinates) of the alien.
+     */
     public void drawAlien(Position position) {
         drawEntity(position.getX(), position.getY(), 'a');
     }
 
+    /**
+     * @brief Draws a new robot using the drawEntity method.
+     * @param position the position (x and y coordinates) of the robot.
+     */
     public void drawRobot(Position position) {
         drawEntity(position.getX(), position.getY(), 'r');
     }
 
+    /**
+     * @brief Draws a new terrain square using the drawEntity method.
+     * @param position the position (x and y coordinates) of the terrain.
+     */
     public void drawTerrain(Position position) {
         drawEntity(position.getX(), position.getY(), 't');
     }
 
+    /**
+     * @brief Draws new text to the back buffer of the GUI.
+     * @param position the position (x and y coordinates) of the text.
+     * @param t the text to be created.
+     * @param color the color of the text.
+     */
     public void drawText(Position position, String t, String color) {
         TextGraphics tg = screen.newTextGraphics();
         tg.setForegroundColor(TextColor.Factory.fromString(color));
--- src/test/java/gui/GUITest.java
+++ src/test/java/gui/GUITest.java
@@ -13,6 +13,9 @@ public class GUITest {
     private GUI gui;
     private TextGraphics tg;
 
+    /**
+     * @brief Pre-requisites for every test.
+     */
     @BeforeEach
     void setUp() {
         screen = Mockito.mock(Screen.class);
@@ -21,18 +24,31 @@ public class GUITest {
         gui = new GUI(screen);
     }
 
+    /**
+     * @brief Tries to draw a new alien and verifies it by checking if the TextGraphics mock called the putString method
+     * once with the 'a' character.
+     */
     @Test
     void drawAlien() {
         gui.drawAlien(new Position(0,0));
         Mockito.verify(tg, Mockito.times(1)).putString(0, 0, "a");
     }
 
+    /**
+     * @brief Tries to draw a new robot and verifies it by checking if the TextGraphics mock called the putString method
+     * once with the 'r' character.
+     */
     @Test
     void drawRobot() {
         gui.drawRobot(new Position(4,5));
         Mockito.verify(tg, Mockito.times(1)).putString(4, 5, "r");
     }
 
+    /**
+     * @brief Tries to draw new text and verifies it by checking if the TextGraphics mock called the putString method
+     * once with the wanted string and if it set the right foreground color by calling the setForegroundColor method
+     * once.
+     */
     @Test
     void drawText() {
         gui.drawText(new Position(1, 1), "Testing drawText() method.", "#000000");
