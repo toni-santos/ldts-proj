ab9f969c9e6e586294525c68a510582cfc81c96f
jonny4547 <joaoapereira21@hotmail.com>
2022-01-15 16:10:01 +0000

    Add more methods to Vector class

--- src/main/java/pt/up/fe/ldts/ootb/util/Vector.java
+++ src/main/java/pt/up/fe/ldts/ootb/util/Vector.java
@@ -1,4 +1,28 @@
 package pt.up.fe.ldts.ootb.util;
 
 public record Vector(int x, int y) {
+
+    public Vector add(Vector other) {
+        return add(other.x, other.y);
+    }
+
+    public Vector add(int x, int y) {
+        return new Vector(this.x + x, this.y + y);
+    }
+
+    public Vector sub(Vector other) {
+        return sub(other.x, other.y);
+    }
+
+    public Vector sub(int x, int y) {
+        return new Vector(this.x - x, this.y - y);
+    }
+
+    public Vector mul(int n) {
+        return new Vector(x*n, y*n);
+    }
+
+    public Vector div(int n) {
+        return new Vector(x/n, y/n);
+    }
 }
