6c0b680a7ae90e06410a70c91c58a62d208a4a56
jonny4547 <joaoapereira21@hotmail.com>
2022-01-15 16:36:16 +0000

    Add SpriteLoader class

---
+++ src/main/java/pt/up/fe/ldts/ootb/gui/render/SpriteLoader.java
@@ -0,0 +1,9 @@
+package pt.up.fe.ldts.ootb.gui.render;
+
+public final class SpriteLoader {
+    public static final Sprite DEFAULT_SPRITE = new Sprite(0, 0, new int[]{});
+
+    public static Sprite load(String path) {
+        return DEFAULT_SPRITE;
+    }
+}
