d1136b7e1876b47029685f2c64b8f4e1673987ec
jonny4547 <joaoapereira21@hotmail.com>
2022-01-16 23:34:23 +0000

    Implement simple states

--- src/main/java/pt/up/fe/ldts/ootb/game/state/InitialState.java
+++ src/main/java/pt/up/fe/ldts/ootb/game/state/InitialState.java
@@ -1,10 +1,43 @@
 package pt.up.fe.ldts.ootb.game.state;
 
+import pt.up.fe.ldts.ootb.App;
 import pt.up.fe.ldts.ootb.game.Game;
+import pt.up.fe.ldts.ootb.gui.input.KeyboardEvent;
 import pt.up.fe.ldts.ootb.gui.input.KeyboardListener;
+import pt.up.fe.ldts.ootb.gui.render.Renderer;
+import pt.up.fe.ldts.ootb.util.GUIUtil;
+import pt.up.fe.ldts.ootb.util.Vector;
 
 public class InitialState extends BaseState implements KeyboardListener {
     public InitialState(Game game) {
         super(game);
+    }
+
+    @Override
+    protected void drawBeforeCreatures(Renderer renderer) {
+
+    }
+
+    @Override
+    protected void drawAfterCreatures(Renderer renderer) {
+        renderer.drawText(new Vector(GUIUtil.WIDTH/2, GUIUtil.HEIGHT/2), "Press <Enter> to start", 20, 0xFFFFFFFF);
+    }
+
+    @Override
+    public void init(App app) {
+        super.init(app);
+        app.getInputHandler().addKeyboardListener(this);
+    }
+
+    @Override
+    public void dispose(App app) {
+        super.dispose(app);
+        app.getInputHandler().removeKeyboardListener(this);
+    }
+
+    @Override
+    public void onKeyboardEvent(KeyboardEvent event) {
+        if (event.character() == '\n')
+            game.changeState(new BeforePlayerState(game));
     }
 }
--- src/main/java/pt/up/fe/ldts/ootb/game/state/LostState.java
+++ src/main/java/pt/up/fe/ldts/ootb/game/state/LostState.java
@@ -1,10 +1,47 @@
 package pt.up.fe.ldts.ootb.game.state;
 
+import pt.up.fe.ldts.ootb.App;
 import pt.up.fe.ldts.ootb.game.Game;
+import pt.up.fe.ldts.ootb.gui.input.KeyboardEvent;
 import pt.up.fe.ldts.ootb.gui.input.KeyboardListener;
+import pt.up.fe.ldts.ootb.gui.render.Renderer;
+import pt.up.fe.ldts.ootb.menu.MainMenu;
+import pt.up.fe.ldts.ootb.util.Vector;
 
 public class LostState extends BaseState implements KeyboardListener {
+    private App app;
+
     public LostState(Game game) {
         super(game);
+    }
+
+    @Override
+    public void init(App app) {
+        super.init(app);
+        app.getInputHandler().addKeyboardListener(this);
+
+        this.app = app;
+    }
+
+    @Override
+    public void dispose(App app) {
+        super.dispose(app);
+        app.getInputHandler().removeKeyboardListener(this);
+    }
+
+    @Override
+    protected void drawBeforeCreatures(Renderer renderer) {
+
+    }
+
+    @Override
+    protected void drawAfterCreatures(Renderer renderer) {
+        renderer.drawText(new Vector(48*4, 48*4), "Press <Enter> to go back", 20, 0xFFFFFFFF);
+    }
+
+    @Override
+    public void onKeyboardEvent(KeyboardEvent event) {
+        if (event.character() == '\n')
+            app.navigate(new MainMenu());
     }
 }
