4975b7fa178c824cf166648a8746e1f5b0e2bed9
jonny4547 <joaoapereira21@hotmail.com>
2022-01-16 22:34:26 +0000

    Game tests passing

--- src/main/java/pt/up/fe/ldts/ootb/game/Game.java
+++ src/main/java/pt/up/fe/ldts/ootb/game/Game.java
@@ -1,10 +1,15 @@
 package pt.up.fe.ldts.ootb.game;
 
+import pt.up.fe.ldts.ootb.game.entity.creature.Creature;
 import pt.up.fe.ldts.ootb.game.entity.creature.alien.Alien;
 import pt.up.fe.ldts.ootb.game.entity.creature.robot.Robot;
+import pt.up.fe.ldts.ootb.game.entity.terrain.Terrain;
 import pt.up.fe.ldts.ootb.game.state.State;
+import pt.up.fe.ldts.ootb.util.Vector;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Game {
     private State state;
@@ -12,10 +17,10 @@ public class Game {
     private final List<Robot> robots;
     private final List<Alien> aliens;
 
-    public Game(Board board, List<Robot> robots, List<Alien> aliens) {
-        this.board = board;
-        this.robots = robots;
-        this.aliens = aliens;
+    public Game(Level level) {
+        board = level.board();
+        robots = level.robots();
+        aliens = level.aliens();
     }
 
     public void changeState(State state) {
@@ -36,5 +41,55 @@ public class Game {
 
     public List<Alien> getAliens() {
         return aliens;
+    }
+
+    public Creature getCreatureAt(Vector position) {
+        for (Creature creature : this.robots)
+            if (creature.getPosition().equals(position))
+                return creature;
+
+        for (Creature creature : this.aliens)
+            if (creature.getPosition().equals(position))
+                return creature;
+
+        return null;
+    }
+
+    public boolean isValidMovement(Creature creature, Vector position) {
+        if (!board.isInsideBoard(position) || creature.getPosition().gridDistance(position) > creature.getMovementRange())
+            return false;
+
+        for (Robot robot: robots) {
+            if (robot.getPosition().equals(position)) {
+                return false;
+            }
+        }
+
+        for (Alien alien: aliens) {
+            if (alien.getPosition().equals(position)) {
+                return false;
+            }
+        }
+
+        return board.terrainAt(position).supportsCreature(creature);
+    }
+
+    public Set<Vector> movableTiles(Creature creature) {
+        Set<Vector> movable = new HashSet<>();
+
+        if (creature != null)
+            for (Terrain terrain: getBoard().terrains())
+                if (isValidMovement(creature, terrain.getPosition()))
+                    movable.add(terrain.getPosition());
+
+        return movable;
+    }
+
+    public int aliveRobots() {
+        return (int) robots.stream().filter(Creature::isAlive).count();
+    }
+
+    public int aliveAliens() {
+        return (int) aliens.stream().filter(Creature::isAlive).count();
     }
 }
