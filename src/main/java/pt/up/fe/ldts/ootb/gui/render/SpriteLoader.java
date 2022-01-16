a21115346805b5008e82cf45b04fb10ab80b6c83
jonny4547 <joaoapereira21@hotmail.com>
2022-01-16 14:36:42 +0000

    Add a cache to sprite loading; load sprite from text

--- src/main/java/pt/up/fe/ldts/ootb/gui/render/SpriteLoader.java
+++ src/main/java/pt/up/fe/ldts/ootb/gui/render/SpriteLoader.java
@@ -1,22 +1,44 @@
 package pt.up.fe.ldts.ootb.gui.render;
 
+import pt.up.fe.ldts.ootb.util.GUIUtil;
+
 import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.font.LineMetrics;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public final class SpriteLoader {
     public static final Sprite DEFAULT_SPRITE;
+    public static final Font FONT;
 
     static {
-        int SIZE = 64;
+        int[] pixels = new int[GUIUtil.TILE_WIDTH*GUIUtil.TILE_HEIGHT];
 
-        int[] pixels = new int[SIZE*SIZE];
-        Arrays.fill(pixels, 0xFFFF00FF);
-        DEFAULT_SPRITE = new Sprite(SIZE, SIZE, pixels);
+        for (int x = 0; x < GUIUtil.TILE_WIDTH; x++)
+            for (int y = 0; y < GUIUtil.TILE_HEIGHT; y++)
+                pixels[x + y*GUIUtil.TILE_WIDTH] = (x-GUIUtil.TILE_WIDTH/2)*(y-GUIUtil.TILE_HEIGHT/2) > 0 ? 0xFF000000 : 0xFFFF00FF;
+
+        DEFAULT_SPRITE = new Sprite(GUIUtil.TILE_WIDTH, GUIUtil.TILE_HEIGHT, pixels);
+
+        Font font;
+
+        try {
+            URL resource = SpriteLoader.class.getClassLoader().getResource("fonts/OCR A Std Regular.ttf");
+            File file = new File(resource.toURI());
+            font = Font.createFont(Font.TRUETYPE_FONT, file);
+        } catch (NullPointerException | IOException | URISyntaxException | FontFormatException e) {
+            System.err.println("Font not found, using default font.");
+            font = Font.decode(Font.SANS_SERIF);
+        }
+
+        FONT = font;
     }
 
     private static final Map<String, Sprite> sprites = new HashMap<>();
@@ -29,18 +51,47 @@ public final class SpriteLoader {
             URL resource = SpriteLoader.class.getClassLoader().getResource("sprites/" + path);
             BufferedImage image = ImageIO.read(resource);
 
-            int width = image.getWidth();
-            int height = image.getHeight();
-
-            int[] pixels = image.getRGB(0, 0, width, height, null, 0, width);
-            Sprite sprite = new Sprite(width, height, pixels);
-
+            Sprite sprite = fromImage(image);
             sprites.put(path, sprite);
-
             return sprite;
         } catch (IOException | NullPointerException | IllegalArgumentException e) {
             System.err.println("Sprite at '" + path + "' was not found, using default sprite.");
             return DEFAULT_SPRITE;
         }
+    }
+
+    public static Sprite fromText(String text, int size, int color) {
+        String path = "$text$." + size + "." + color + "." + text;
+
+        if (sprites.containsKey(path))
+            return sprites.get(path);
+
+        Font font = FONT.deriveFont((float) size);
+
+        FontRenderContext frc = new FontRenderContext(null, false, false);
+        LineMetrics metrics = font.getLineMetrics(text, frc);
+
+        int width = (int) Math.ceil(font.getStringBounds(text, frc).getWidth());
+        int height = (int) Math.ceil(metrics.getHeight());
+        int ascent = (int) Math.ceil(metrics.getAscent());
+
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics graphics = image.getGraphics();
+        graphics.setColor(new Color(color & 0xFFFFFF));
+        graphics.setFont(font);
+        graphics.drawString(text, 0, ascent);
+
+        Sprite sprite = fromImage(image);
+        sprites.put(path, sprite);
+        return sprite;
+    }
+
+    private static Sprite fromImage(BufferedImage image) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        int[] pixels = image.getRGB(0, 0, width, height, null, 0, width);
+        return new Sprite(width, height, pixels);
     }
 }
