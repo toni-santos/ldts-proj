61eb0e77d551fa4ef103cd0d4788136db1045390
jonny4547 <joaoapereira21@hotmail.com>
2022-01-16 23:57:46 +0000

    Implement BeforePlayerState

--- src/main/java/pt/up/fe/ldts/ootb/game/state/BeforePlayerState.java
+++ src/main/java/pt/up/fe/ldts/ootb/game/state/BeforePlayerState.java
@@ -1,9 +1,35 @@
 package pt.up.fe.ldts.ootb.game.state;
 
+import pt.up.fe.ldts.ootb.App;
 import pt.up.fe.ldts.ootb.game.Game;
+import pt.up.fe.ldts.ootb.game.entity.creature.robot.Robot;
+import pt.up.fe.ldts.ootb.gui.render.Renderer;
 
 public class BeforePlayerState extends BaseState {
     public BeforePlayerState(Game game) {
         super(game);
+    }
+
+    @Override
+    public void init(App app) {
+        super.init(app);
+
+        for (Robot robot : game.getRobots()) {
+            if (robot.isAlive()) {
+                robot.setPosition(robot.getStrategy().move(robot, game));
+                robot.getStrategy().planAttack(robot, game);
+            }
+        }
+        game.changeState(new MoveState(game));
+    }
+
+    @Override
+    protected void drawBeforeCreatures(Renderer renderer) {
+
+    }
+
+    @Override
+    protected void drawAfterCreatures(Renderer renderer) {
+
     }
 }
