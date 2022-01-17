05d32166ce3c7bda7f739e2b05bff104975a053d
jonny4547 <joaoapereira21@hotmail.com>
2022-01-17 02:18:23 +0000

    Implement MoveState and AttackState

--- src/main/java/pt/up/fe/ldts/ootb/game/state/AttackState.java
+++ src/main/java/pt/up/fe/ldts/ootb/game/state/AttackState.java
@@ -1,9 +1,73 @@
 package pt.up.fe.ldts.ootb.game.state;
 
 import pt.up.fe.ldts.ootb.game.Game;
+import pt.up.fe.ldts.ootb.game.entity.creature.alien.Alien;
+import pt.up.fe.ldts.ootb.gui.render.Renderer;
+import pt.up.fe.ldts.ootb.util.GUIUtil;
+import pt.up.fe.ldts.ootb.util.Vector;
+
+import java.util.Set;
 
 public class AttackState extends BasePlayerState {
+
     public AttackState(Game game) {
         super(game);
+    }
+
+    @Override
+    protected boolean interact(Alien alien, Vector position) {
+        if (!alien.isAlive()
+         || !alien.getAbility().getTilesInRange(alien.getPosition(), game).contains(position))
+            return false;
+
+        alien.getAbility().execute(
+                alien.getPosition(),
+                alien.getAbility().getAffectedTiles(
+                        alien.getPosition(),
+                        position,
+                        game)
+                ,game);
+
+        return true;
+    }
+
+    @Override
+    protected void nextState() {
+        game.changeState(new AfterPlayerState(game));
+    }
+
+    @Override
+    protected Set<Vector> previewTiles(Alien alien) {
+        return alien.getAbility().getTilesInRange(alien.getPosition(), game);
+    }
+
+    @Override
+    protected int previewColor() {
+        return 0xCC0000;
+    }
+
+    @Override
+    protected Set<Vector> selectedTiles(Alien alien) {
+        return alien.getAbility().getTilesInRange(alien.getPosition(), game);
+    }
+
+    @Override
+    protected int selectedColor() {
+        return 0xFF0000;
+    }
+
+    @Override
+    protected Set<Vector> hoveredTiles(Alien alien, Vector hovered) {
+        return alien.getAbility().getAffectedTiles(alien.getPosition(), hovered, game);
+    }
+
+    @Override
+    protected int hoveredColor() {
+        return 0xFF00CC;
+    }
+
+    @Override
+    protected void drawAfterCreatures(Renderer renderer) {
+        renderer.drawText(new Vector(GUIUtil.WIDTH/2, GUIUtil.HEIGHT/2), "Attack stage", 20, 0xFFFFFFFF);
     }
 }
--- src/main/java/pt/up/fe/ldts/ootb/game/state/MoveState.java
+++ src/main/java/pt/up/fe/ldts/ootb/game/state/MoveState.java
@@ -1,10 +1,66 @@
 package pt.up.fe.ldts.ootb.game.state;
 
 import pt.up.fe.ldts.ootb.game.Game;
+import pt.up.fe.ldts.ootb.game.entity.creature.alien.Alien;
+import pt.up.fe.ldts.ootb.gui.render.Renderer;
+import pt.up.fe.ldts.ootb.util.GUIUtil;
+import pt.up.fe.ldts.ootb.util.Vector;
+
+import java.util.Collections;
+import java.util.Set;
 
 public class MoveState extends BasePlayerState {
 
     public MoveState(Game game) {
         super(game);
+    }
+
+    @Override
+    protected boolean interact(Alien alien, Vector position) {
+        if (!game.isValidMovement(alien, position))
+            return false;
+
+        alien.setPosition(position);
+        return true;
+    }
+
+    @Override
+    protected void nextState() {
+        game.changeState(new AttackState(game));
+    }
+
+    @Override
+    protected Set<Vector> previewTiles(Alien alien) {
+        return game.movableTiles(alien);
+    }
+
+    @Override
+    protected int previewColor() {
+        return 0x00CC00;
+    }
+
+    @Override
+    protected Set<Vector> selectedTiles(Alien alien) {
+        return game.movableTiles(alien);
+    }
+
+    @Override
+    protected int selectedColor() {
+        return 0x00FF00;
+    }
+
+    @Override
+    protected Set<Vector> hoveredTiles(Alien alien, Vector hovered) {
+        return Collections.singleton(hovered);
+    }
+
+    @Override
+    protected int hoveredColor() {
+        return 0x00FF44;
+    }
+
+    @Override
+    protected void drawAfterCreatures(Renderer renderer) {
+        renderer.drawText(new Vector(GUIUtil.WIDTH/2, GUIUtil.HEIGHT/2), "Move stage", 20, 0xFFFFFFFF);
     }
 }
